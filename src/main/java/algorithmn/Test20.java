ceba9612357350c358cf612777805f05fcd73b26
jsjune <avraskio@gmail.com>
2022-03-14 14:10:19 +0900

    initial commit

--- src/main/java/algorithmn/Test20.java
+++ src/main/java/algorithmn/Test20.java
@@ -1,11 +1,36 @@
 package algorithmn;
 
-public class Test20 {
-    public static void main(String[] args) {
-        String[] par = {"leo", "kiki", "eden"};
-        String[] com = {"eden", "kiki"};
-        for (int i = 0; i < par.length; i++) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
+public class Test20 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
         }
+        return answer;
     }
+
+    public static void main(String[] args) {
+        String[] par = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] com = {"josipa", "filipa", "marina", "nikola"};
+        Test20 test = new Test20();
+        System.out.println(test.solution(par,com));
+    }
+
+
 }
+
+//        Arrays.sort(participatn);
+//                Arrays.sort(completion);
+//                int i = 0;
+//                for (i = 0; i < completion.length; i++) {
+//        if (!participant[i].equals(completion[i])) {
+//        break;
+//        }
+//        }
+//        System.out.println(par[i]);

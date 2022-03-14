b8ac312a32184d81b5e6c41b324c088f5aa5c43a
jsjune <avraskio@gmail.com>
2022-03-14 16:54:21 +0900

    initial commit

---
+++ src/main/java/algorithmn/Test26.java
@@ -0,0 +1,30 @@
+package algorithmn;
+
+public class Test26 {
+//    public int[] solution(int[] arr) {
+//        if (arr.length == 1) {
+//            arr[0] = -1;
+//        } else {
+//            int[] answer = new int[arr.length - 1];
+//            for (int i = 0; i < answer.length; i++) {
+//                if (arr[i + 1] > arr[i]) {
+//                    answer[i] = arr[i + 1];
+//                } else {
+//                    answer[i] = arr[i];
+//                }
+//            }
+//            return answer;
+//        }
+//        return ;
+//    }
+
+    public static void main(String[] args) {
+        Test26 test = new Test26();
+        int[] arr = {4, 3, 2, 1};
+//        System.out.println(test.solution(arr));
+        int[] arrr = {};
+        for (int i = 0; i < arr.length; i++) {
+            arrr[i]=arr[i];
+        }
+    }
+}

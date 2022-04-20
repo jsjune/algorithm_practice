0df87da07c40e93bca906b4476aa802902281ed1
jsjune <avraskio@gmail.com>
2022-04-20 17:40:18 +0900

    update

---
+++ src/main/java/algorithm/Test5.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+public class Test5 {
+    public static void main(String[] args) {
+        int a=25;
+        int b=7;
+        if (b > 5) {
+            a=30;
+        }
+        System.out.println(a);
+    }
+}
